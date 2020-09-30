53953e32d1c491e9895a49a71906786ffbb91c65
Cyril Meyer <contact@cyrilmeyer.eu>
2020-09-30 18:23:22 +0200

    fix data/patch, for 2.5D patches

--- data/patch.py
+++ data/patch.py
@@ -298,43 +298,42 @@ def predict_slice_one_hot_tos_area_p(patch_size, image, model, lambdas=[]):
 
 
 # 2.5D Patches
-def gen_patches_batch_augmented_25d_label_indexes_one_hot(image, label, label_indexes, batch_size=32, z_slices=3):
+def gen_patches_batch_augmented_25d_label_indexes_one_hot(patch_size, image, label, label_indexes, batch_size=32, z_slices=3):
     # valid z_slices is odd
     z_d2 = z_slices//2
     n_label = label[0].shape[-1]
     label_indexes_iter = []
-    for d in range(len(label_indexes)):
-        label_indexes_iter.append(itertools.cycle(label_indexes[d]))
-    batch_image = np.zeros((batch_size, PATCH_SIZE, PATCH_SIZE, z_d2*2+1))
-    batch_label = np.zeros((batch_size, PATCH_SIZE, PATCH_SIZE, n_label))
-                            
+    for cla in range(len(label_indexes)):
+        label_indexes_iter.append(itertools.cycle(label_indexes[cla]))
+    batch_image = np.zeros((batch_size, patch_size, patch_size, z_d2*2+1))
+    batch_label = np.zeros((batch_size, patch_size, patch_size, n_label))
+             
     while True:
         for i in range(batch_size):
             # 20% of random patches
             if randint(0,9) < 2:
-                d = randint(0, len(image)-1)
-                x = randint(0, image[d].shape[2] - PATCH_SIZE - 1)
-                y = randint(0, image[d].shape[1] - PATCH_SIZE - 1)
-                z = randint(z_d2, image[d].shape[0] - 1 - z_d2)
+                x = randint(0, image.shape[2] - patch_size - 1)
+                y = randint(0, image.shape[1] - patch_size - 1)
+                z = randint(z_d2, image.shape[0] - 1 - z_d2)
             else:
-                d = randint(0, len(label_indexes)-1)
+                cla = randint(0, len(label_indexes)-1)
+                
+                z, y, x = next(label_indexes_iter[cla])
+                y = max(0, y - 1 - (patch_size // 2))
+                x = max(0, x - 1 - (patch_size // 2))
 
-                z, y, x = next(label_indexes_iter[d])
-                y = max(0, y - 1 - (PATCH_SIZE // 2))
-                x = max(0, x - 1 - (PATCH_SIZE // 2))
-
-                y = min(max(0, y), image[d].shape[1]-PATCH_SIZE)
-                x = min(max(0, x), image[d].shape[2]-PATCH_SIZE)
+                y = min(max(0, y), image.shape[1]-patch_size)
+                x = min(max(0, x), image.shape[2]-patch_size)
                 
                 z = max(z_d2, z)
-                z = min(z, image[d].shape[0] - 1 - z_d2)
+                z = min(z, image.shape[0] - 1 - z_d2)
             
-            batch_image[i, :, :, z_d2] = image[d][z, y:y + PATCH_SIZE, x:x + PATCH_SIZE]
+            batch_image[i, :, :, z_d2] = image[z, y:y + patch_size, x:x + patch_size]
             for j in range(1, z_d2+1):
-                batch_image[i, :, :, z_d2-j] = image[d][z-j, y:y + PATCH_SIZE, x:x + PATCH_SIZE]
-                batch_image[i, :, :, z_d2+j] = image[d][z+j, y:y + PATCH_SIZE, x:x + PATCH_SIZE]
+                batch_image[i, :, :, z_d2-j] = image[z-j, y:y + patch_size, x:x + patch_size]
+                batch_image[i, :, :, z_d2+j] = image[z+j, y:y + patch_size, x:x + patch_size]
                 
-            batch_label[i, :, :, :] = label[d][z, y:y + PATCH_SIZE, x:x + PATCH_SIZE]
+            batch_label[i, :, :, :] = label[z, y:y + patch_size, x:x + patch_size]
             
             rot = randint(0, 3)
             batch_image[i, :, :] = np.rot90(batch_image[i, :, :], rot)
@@ -351,16 +350,16 @@ def gen_patches_batch_augmented_25d_label_indexes_one_hot(image, label, label_in
         yield batch_image, batch_label
 
 
-def predict_slice_one_hot_25d(image, model, z_slices):
+def predict_slice_one_hot_25d(patch_size, image, model, z_slices):
     # valid z_slices is odd
     z_d2 = z_slices//2
     current_image = np.expand_dims(image, -1)
-    prediction_stride = PATCH_SIZE // 3
+    prediction_stride = patch_size // 3
     
-    pad_y0 = PATCH_SIZE
-    pad_y1 = PATCH_SIZE
-    pad_x0 = PATCH_SIZE
-    pad_x1 = PATCH_SIZE
+    pad_y0 = patch_size
+    pad_y1 = patch_size
+    pad_x0 = patch_size
+    pad_x1 = patch_size
     
     pad_y0 += 0 if (current_image[0].shape[0] % prediction_stride == 0) else math.floor((prediction_stride - current_image[0].shape[0] % prediction_stride)/2)
     pad_y1 += 0 if (current_image[0].shape[0] % prediction_stride == 0) else math.ceil((prediction_stride - current_image[0].shape[0] % prediction_stride)/2)
@@ -373,17 +372,17 @@ def predict_slice_one_hot_25d(image, model, z_slices):
         current_image_padded[z] = skimage.util.pad(current_image[z], pad_width=((pad_y0,pad_y1),(pad_x0, pad_x1),(0,0)), mode='symmetric')
     
     batch_size = (current_image_padded.shape[1] // prediction_stride) * (current_image_padded.shape[2] // prediction_stride)
-    patches_batch = np.zeros((batch_size,PATCH_SIZE,PATCH_SIZE,z_d2*2+1))
+    patches_batch = np.zeros((batch_size,patch_size,patch_size,z_d2*2+1))
     prediction_image = np.zeros((current_image_padded.shape[1], current_image_padded.shape[2]), dtype=image.dtype)
         
     # deconstruct
     p = 0
-    for y in range(0, current_image_padded.shape[1]-PATCH_SIZE-1, prediction_stride):
-        for x in range(0, current_image_padded.shape[2]-PATCH_SIZE-1, prediction_stride):
-            patches_batch[p, :, :, z_d2] = current_image_padded[z_d2, y:y + PATCH_SIZE, x:x + PATCH_SIZE, 0]
+    for y in range(0, current_image_padded.shape[1]-patch_size-1, prediction_stride):
+        for x in range(0, current_image_padded.shape[2]-patch_size-1, prediction_stride):
+            patches_batch[p, :, :, z_d2] = current_image_padded[z_d2, y:y + patch_size, x:x + patch_size, 0]
             for j in range(1, z_d2+1):
-                patches_batch[p, :, :, z_d2-j] = current_image_padded[z_d2-j, y:y + PATCH_SIZE, x:x + PATCH_SIZE, 0]
-                patches_batch[p, :, :, z_d2+j] = current_image_padded[z_d2+j, y:y + PATCH_SIZE, x:x + PATCH_SIZE, 0]
+                patches_batch[p, :, :, z_d2-j] = current_image_padded[z_d2-j, y:y + patch_size, x:x + patch_size, 0]
+                patches_batch[p, :, :, z_d2+j] = current_image_padded[z_d2+j, y:y + patch_size, x:x + patch_size, 0]
             p = p + 1
     
     # predict
@@ -394,8 +393,8 @@ def predict_slice_one_hot_25d(image, model, z_slices):
     # reconstruct
     ps = prediction_stride
     p = 0
-    for y in range(0, current_image_padded.shape[1]-PATCH_SIZE-1, prediction_stride):
-        for x in range(0, current_image_padded.shape[2]-PATCH_SIZE-1, prediction_stride):
+    for y in range(0, current_image_padded.shape[1]-patch_size-1, prediction_stride):
+        for x in range(0, current_image_padded.shape[2]-patch_size-1, prediction_stride):
             prediction_patch = patches_batch[p]
             p = p + 1
             prediction_image[y+ps:y+2*ps,x+ps:x+2*ps] = prediction_patch[ps:2*ps,ps:2*ps]
