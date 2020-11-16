49cce3c7cdaaf15a82fdbfd0f47e8eabe8883f22
Cyril Meyer <contact@cyrilmeyer.eu>
2020-11-16 16:08:23 +0100

    add new option for UNet models

--- models/UNet3D.py
+++ models/UNet3D.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
 
-from tensorflow.keras.layers import BatchNormalization, Conv2D, Cropping2D, Conv2DTranspose, concatenate
-from tensorflow.keras.layers import Dropout, MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization, Conv2D, Conv3D, Cropping2D, Conv3DTranspose, concatenate
+from tensorflow.keras.layers import Dropout, MaxPooling3D
 
 
 def conv_block(input, filters, kernel_size=3, conv_per_block=2, padding='same', dropout=0, batch_normalization=False, groups=1):
@@ -10,7 +10,7 @@ def conv_block(input, filters, kernel_size=3, conv_per_block=2, padding='same', 
     X = input
 
     for i in range(conv_per_block):
-        X = Conv2D(filters=filters, kernel_size=kernel_size,
+        X = Conv3D(filters=filters, kernel_size=kernel_size,
                    activation='relu', kernel_initializer="he_normal", padding=padding, groups=groups)(X)
         if batch_normalization:
             X = BatchNormalization()(X)
@@ -21,8 +21,8 @@ def conv_block(input, filters, kernel_size=3, conv_per_block=2, padding='same', 
     return X
 
 
-def UNet(input_shape=(None, None, None), output_classes=2, filters=64, depth=5, conv_per_block=2,
-         padding='same', dropouts=0.50, batch_normalization=False, groups=1, input_conv=False):
+def UNet(input_shape=(None, None, None, None), output_classes=2, filters=64, depth=5, conv_per_block=2,
+         padding='same', dropouts=0.50, batch_normalization=False, groups=1):
     """
     :param input_shape: input shape tuple
     :param output_classes: number of output classes (single output for output_classes <= 2)
@@ -78,23 +78,20 @@ def UNet(input_shape=(None, None, None), output_classes=2, filters=64, depth=5, 
         groups = np.ones((2 * depth - 1), dtype=np.uint8)
 
     inputs = tf.keras.Input(input_shape, name="input")
-    
+
     # output of each block
     block_out = []
     # X represent the previous output
     X = inputs
     
-    if input_conv:
-        X = Conv2D(filters=input_shape[-1], kernel_size=1, activation=None)(X)
-        # X = Conv2D(filters=input_shape[-1], kernel_size=1, activation='relu', kernel_initializer="he_normal", padding="same")(X)
-
+    X = Conv2D(filters=3, kernel_size=filters[0], activation='relu', kernel_initializer="he_normal", padding='same')(X)
 
     # Contracting path
     for i in range(depth-1):
         X = conv_block(X, filters[i], 3, conv_per_block, padding, dropouts[i], batch_normalization, groups=groups[i])
         block_out.append(X)
 
-        X = MaxPooling2D(pool_size=(2, 2))(X)
+        X = MaxPooling3D(pool_size=(2, 2, 1))(X)
 
     X = conv_block(X, filters[depth-1], 3, conv_per_block, padding, dropouts[depth-1], batch_normalization, groups=groups[depth-1])
 
@@ -102,7 +99,7 @@ def UNet(input_shape=(None, None, None), output_classes=2, filters=64, depth=5, 
     crop = 2
     for i in range(depth-1):
         # X = tf.keras.layers.UpSampling2D()(X)
-        X = Conv2DTranspose(filters[depth+i], 2, 2, padding='valid')(X)
+        X = Conv3DTranspose(filters[depth+i], 2, 2, padding='valid')(X)
 
         if padding == 'valid':
             block_out[depth - i - 2] = Cropping2D(crop * conv_per_block)(block_out[depth - i - 2])
@@ -114,9 +111,9 @@ def UNet(input_shape=(None, None, None), output_classes=2, filters=64, depth=5, 
         X = conv_block(X, filters[depth+i], 3, conv_per_block, padding, dropouts[depth+i], batch_normalization, groups=groups[depth+i])
 
     if output_classes > 2:
-        tmp = Conv2D(output_classes, 1, activation='softmax', name="output")(X)
+        tmp = Conv3D(output_classes, 1, activation='softmax', name="output")(X)
     else:
-        tmp = Conv2D(1, 1, activation='sigmoid', name="output")(X)
+        tmp = Conv3D(1, 1, activation='sigmoid', name="output")(X)
 
     model = tf.keras.Model(inputs=inputs, outputs=tmp)
 
