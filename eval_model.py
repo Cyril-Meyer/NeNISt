1ed2848eba022c033365e8ab06143bb6bc89f6e7
Cyril Meyer <contact@cyrilmeyer.eu>
2022-06-21 16:39:59 +0200

    current train/eval system, need to work on MSEDT training

---
+++ eval_model.py
@@ -0,0 +1,118 @@
+import os
+import time
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from lii.lii import LargeImageInference as lii
+
+import utils.parser
+import datasets.reader
+import loss.segmentation
+import metrics.segmentation
+import medpy.metric.binary
+
+t0 = time.time()
+args = utils.parser.args_eval()
+
+
+def log(*strings):
+    print(str(int(time.time() - t0)).rjust(10), *strings)
+
+
+OUTPUT_FOLDER = args.save_folder
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
+log("OUTPUT FOLDER :", OUTPUT_FOLDER)
+
+
+# load data
+log("DATA : Loading")
+images, labels = datasets.reader.get_data(args.images, args.labels, create_background=args.create_background)
+if args.create_background:
+    assert np.all(np.sum(labels[0], axis=-1) == 1)
+output_classes = labels[0].shape[-1]
+
+
+log("LOAD MODEL :", args.load_model)
+custom_objects = {"dice_coef_tf_meyer": loss.segmentation.dice_coef_tf_meyer}
+with tf.keras.utils.custom_object_scope(custom_objects):
+    model = tf.keras.models.load_model(args.load_model)
+
+if args.verbose:
+    log('input shape   :', model.input_shape)
+    log('output shape  :', model.output_shape)
+    log('parameters    :', model.count_params())
+    log('model name    :', model.name)
+
+assert len(model.input_shape) == len(model.output_shape)
+
+predictions = []
+# with tf.device("cpu:0"):
+# with tf.device("gpu:0"):
+for i in range(len(images)):
+    image = images[i]
+    pred = model
+    # Y / X padding
+    f = args.patch_size[-2]
+    yp = 0 if image.shape[1] % f == 0 else f * (image.shape[1] // f + 1) - image.shape[1]
+    f = args.patch_size[-1]
+    xp = 0 if image.shape[2] % f == 0 else f * (image.shape[2] // f + 1) - image.shape[2]
+    image_p = np.pad(image, pad_width=[(0, 0), (0, yp), (0, xp), (0, 0)], mode='reflect')
+
+    if len(model.input_shape) == 4:
+        overlap = (1, 2, 2)
+        def pred_2d(img): return np.expand_dims(model(img[:, 0, :, :, :]), axis=1)
+        pred = pred_2d
+        predictions.append(
+            lii.infer(image_p, (1, ) + tuple(args.patch_size), pred, overlap, verbose=int(args.verbose), dtype=np.float32)[:, 0:image.shape[1], 0:image.shape[2], :])
+    elif len(model.input_shape) == 5:
+        overlap = (1, 2, 2) if image.shape[0] == args.patch_size[0] else (2, 2, 2)
+        predictions.append(
+            lii.infer(image_p, args.patch_size, pred, overlap, verbose=int(args.verbose), dtype=np.float32)[:, 0:image.shape[1], 0:image.shape[2], :])
+    else:
+        raise NotImplementedError
+
+
+for i in range(len(images)):
+    if args.name_prefix is None:
+        name = os.path.basename(args.images[i]).split('.')[0]
+    else:
+        name = args.name_prefix + os.path.basename(args.images[i]).split('.')[0]
+    log('name', name)
+
+    file = open(f"{OUTPUT_FOLDER}/{name}.csv", "w")
+    file.write("name,")
+    for c in range(predictions[i].shape[-1]):
+        file.write(f'precision_{c+1},recall_{c+1},f1_{c+1},iou_{c+1},assd_{c+1}\n')
+    file.write(f'{name},')
+
+    prediction = predictions[i] > args.binary_threshold
+    label = labels[i] > 0.5
+
+    for c in range(prediction.shape[-1]):
+        precision = metrics.segmentation.precision(label.flatten(), prediction.flatten())
+        recall    = metrics.segmentation.recall(label.flatten(), prediction.flatten())
+        f1        = metrics.segmentation.F1(label.flatten(), prediction.flatten())
+        iou       = metrics.segmentation.IoU(label.flatten(), prediction.flatten())
+        assd      = medpy.metric.binary.assd(label, prediction)
+        # precision = sklearn.metrics.precision_score(label.flatten(), prediction.flatten())
+        # recall    = sklearn.metrics.recall_score(label.flatten(), prediction.flatten())
+        # f1        = sklearn.metrics.f1_score(label.flatten(), prediction.flatten())
+        # iou       = sklearn.metrics.jaccard_score(label.flatten(), prediction.flatten())
+        file.write(f'{precision},{recall},{f1},{iou},{assd}\n')
+
+        if args.verbose:
+            log('precision :', precision)
+            log('recall    :', recall)
+            log('f1        :', f1)
+            log('iou       :', iou)
+            log('assd      :', assd)
+            '''
+            plt.imshow(label[0, :, :, c].astype(np.float32))
+            plt.show()
+            plt.imshow(prediction[0, :, :, c].astype(np.float32))
+            plt.show()
+            '''
+    file.close()
--- train_model.py
+++ train_model.py
@@ -9,28 +9,42 @@ import datasets.reader
 import data.patch
 import data.distance_transform
 import models.unet
+import loss.segmentation
 
 t0 = time.time()
+args = utils.parser.args_train()
 
-args = utils.parser.args()
 
-OUTPUT_FOLDER = args.save
+def log(*strings):
+    print(str(int(time.time() - t0)).rjust(10), *strings)
+
+
+OUTPUT_FOLDER = args.save_folder
 if not os.path.exists(OUTPUT_FOLDER):
     os.makedirs(OUTPUT_FOLDER)
-print(str(int(time.time() - t0)).rjust(10), "OUTPUT FOLDER :", OUTPUT_FOLDER)
+log("OUTPUT FOLDER :", OUTPUT_FOLDER)
 
+# activation
+activation = args.activation
 # loss
-loss, activation, multiple_outputs, threshold = utils.parser.get_loss(args.loss)
+loss = utils.parser.get_loss(args.loss, activation)
 
 # load data
-print(str(int(time.time() - t0)).rjust(10), "LOAD DATA")
-images, labels = datasets.reader.get_data(args.images, args.labels)
-images_validation, labels_validation = datasets.reader.get_data(args.images_validation, args.labels_validation)
+log("DATA : Loading")
+images, labels = datasets.reader.get_data(args.images, args.labels, create_background=args.create_background)
+images_validation, labels_validation = datasets.reader.get_data(args.images_validation, args.labels_validation, create_background=args.create_background)
+if args.create_background:
+    assert np.all(np.sum(labels[0], axis=-1) == 1)
 output_classes = labels[0].shape[-1]
+
+log("DATA : Labels id")
+labels_id = datasets.reader.get_labels_id(labels)
+labels_validation_id = datasets.reader.get_labels_id(labels_validation)
 
 # label distance transform
 '''
 if '_dt' in args.loss:
+    log("DATA : Distance transform")
     print(str(int(time.time() - t0)).rjust(10), "EDT DATA")
     edt = data.distance_transform.label_edt
     normalize = data.distance_transform.normalize_tanh
@@ -43,33 +57,38 @@ if '_dt' in args.loss:
 
     labels = labels_dt
 '''
+log("DATA : Ready")
+
+model = utils.parser.get_model(args.model, output_classes, activation)
+log("MODEL :", model.input_shape, "->", model.output_shape)
+log("MODEL :", model.name, f"{model.count_params():,}")
+log("MODEL :", activation, loss)
 
 # patch generator
-if multiple_outputs:
-    train = data.patch.gen_to_multiple_outputs(
-        data.patch.gen_patch_batch(args.patch_size, images, labels, args.batch_size, augmentation=True))
-else:
-    train = data.patch.gen_patch_batch(args.patch_size, images, labels, args.batch_size, augmentation=True)
-
-# create model
-# 3D models
-if args.model == 'unet-4-32' and len(args.patch_size) == 3:
-    model = models.unet.get(input_shape=(None, None, None, 1), output_classes=output_classes, output_activation=activation, filters=32, depth=4, multiple_outputs=multiple_outputs)
-elif args.model == 'unet-5-16' and len(args.patch_size) == 3:
-    model = models.unet.get(input_shape=(None, None, None, 1), output_classes=output_classes, output_activation=activation, filters=16, depth=5, multiple_outputs=multiple_outputs)
-# 2D models
-elif args.model == 'unet-5-26' and len(args.patch_size) == 2:
-    model = models.unet.get(input_shape=(      None, None, 1), output_classes=output_classes, output_activation=activation, filters=26, depth=5, multiple_outputs=multiple_outputs)
-else:
-    raise NotImplementedError
-# model.summary(line_length=300)
+train = data.patch.gen_patch_batch(args.patch_size, images, labels, args.batch_size, augmentation=True,
+                                   label_indexes=labels_id, label_indexes_prop=0.8)
+valid = data.patch.gen_patch_batch(args.patch_size, images_validation, labels_validation, args.batch_size, augmentation=True,
+                                   label_indexes=labels_validation_id, label_indexes_prop=0.8)
+if args.multiple_outputs:
+    train_ = train
+    valid_ = valid
+    train = data.patch.gen_to_multiple_outputs(train_)
+    valid = data.patch.gen_to_multiple_outputs(valid_)
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07)
-model.compile(optimizer=optimizer, loss=loss, loss_weights=[1/output_classes for i in range(output_classes)])
+if args.multiple_outputs:
+    model.compile(optimizer=optimizer, loss=loss, loss_weights=[1/output_classes for i in range(output_classes)])
+else:
+    model.compile(optimizer=optimizer, loss=loss)
 
-fit_history = model.fit(train, steps_per_epoch=args.steps_per_epoch, epochs=args.epochs, verbose=args.verbose)
+log("MODEL : Ready")
 
-model.save(OUTPUT_FOLDER + "/model_last", save_format='h5')
+savebestmodel = tf.keras.callbacks.ModelCheckpoint(filepath=OUTPUT_FOLDER + "/model_best.h5",
+                                                   save_weights_only=False, monitor='val_loss', mode='min',
+                                                   save_best_only=True, verbose=args.verbose)
 
-'''
-'''
+fit_history = model.fit(train, steps_per_epoch=args.steps_per_epoch, epochs=args.epochs, verbose=args.verbose,
+                        validation_data=valid, validation_steps=args.steps_per_epoch_validation,
+                        callbacks=[savebestmodel])
+
+model.save(OUTPUT_FOLDER + "/model_last.h5", save_format='h5')
