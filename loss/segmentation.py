04d43fd12e7ab39abcc3d0e51fbe4c792d489f20
Cyril Meyer <contact@cyrilmeyer.eu>
2022-06-21 16:40:54 +0200

    add new loss versions, need to be cleaned

--- loss/segmentation.py
+++ loss/segmentation.py
@@ -2,6 +2,39 @@ from keras import backend as K
 import tensorflow as tf
 
 
+def dice_coef_tf_meyer(y_true, y_pred):
+    y_true_f = tf.reshape(y_true, [-1])
+    y_pred_f = tf.reshape(y_pred, [-1])
+    intersection = tf.reduce_sum(tf.multiply(y_true_f, y_pred_f))
+    smooth = 0.0001
+    return 1 - (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
+
+
+def dice_coef_multi_tf_meyer(y_true, y_pred):
+    n_label = 4
+    dice = 0
+    for index in range(n_label):
+        dice += dice_coef_tf_meyer(y_true[...,index], y_pred[...,index])
+    return dice / n_label
+
+
+# https://stackoverflow.com/questions/61488732/how-calculate-the-dice-coefficient-for-multi-class-segmentation-task-using-pytho
+def dice_coef_zabir(y_true, y_pred):
+    y_true_f = tf.reshape(y_true, [-1])
+    y_pred_f = tf.reshape(y_pred, [-1])
+    intersection = tf.reduce_sum(tf.multiply(y_true_f, y_pred_f))
+    smooth = 0.0001
+    return 1 - (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
+
+
+def dice_coef_multilabel_zabir(y_true, y_pred):
+    numLabels = 4
+    dice = 0
+    for index in range(numLabels):
+        dice += dice_coef_zabir(y_true[...,index], y_pred[...,index])
+    return dice / numLabels
+
+
 def jaccard_distance_loss(y_true, y_pred, smooth=100):
     """
     Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
@@ -53,3 +86,48 @@ def dice_loss_lars76(y_true, y_pred):
     denominator = tf.reduce_sum(y_true + y_pred)
 
     return 1 - numerator / denominator
+
+
+# https://www.kaggle.com/code/bigironsphere/loss-function-library-keras-pytorch/notebook
+def DiceLoss(targets, inputs, smooth=1e-6):
+    inputs = K.flatten(inputs)
+    targets = K.flatten(targets)
+
+    intersection = K.sum(K.dot(targets, inputs))
+    dice = (2 * intersection + smooth) / (K.sum(targets) + K.sum(inputs) + smooth)
+    return 1 - dice
+
+
+def DiceBCELoss(targets, inputs, smooth=1e-6):
+    inputs = K.flatten(inputs)
+    targets = K.flatten(targets)
+
+    BCE = K.binary_crossentropy(targets, inputs)
+    intersection = K.sum(K.dot(targets, inputs))
+    dice_loss = 1 - (2 * intersection + smooth) / (K.sum(targets) + K.sum(inputs) + smooth)
+    Dice_BCE = BCE + dice_loss
+
+    return Dice_BCE
+
+
+def IoULoss(targets, inputs, smooth=1e-6):
+    inputs = K.flatten(inputs)
+    targets = K.flatten(targets)
+
+    intersection = K.sum(K.dot(targets, inputs))
+    total = K.sum(targets) + K.sum(inputs)
+    union = total - intersection
+
+    IoU = (intersection + smooth) / (union + smooth)
+    return 1 - IoU
+
+
+def FocalLoss(targets, inputs, alpha=0.8, gamma=2):
+    inputs = K.flatten(inputs)
+    targets = K.flatten(targets)
+
+    BCE = K.binary_crossentropy(targets, inputs)
+    BCE_EXP = K.exp(-BCE)
+    focal_loss = K.mean(alpha * K.pow((1 - BCE_EXP), gamma) * BCE)
+
+    return focal_loss
